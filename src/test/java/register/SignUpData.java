7976e96360ad663d6f2ede59ab931bc5c9aa2b68
mohamed <mohamedmok2411994@gmail.com>
2023-07-22 16:25:36 +0300

    updateData

--- src/test/java/register/SignUpData.java
+++ src/test/java/register/SignUpData.java
@@ -7,8 +7,8 @@ public class SignUpData {
     public static Object [] [] signInData ()
     {
         Object [] [] data = new Object [1] [3];
-        data[0][0]="mohamedalyyywgyrygy9298762711156614218@gmail.com";
-        data[0][1]="1123654099";
+        data[0][0]="mohamedalyyywgyr2ygy9298762711156614218@gmail.com";
+        data[0][1]="11236540999";
         data[0][2]=true;
         return data;
 
